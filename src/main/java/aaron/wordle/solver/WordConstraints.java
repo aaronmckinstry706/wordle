6e0e4661dba9404f4e4e8e159a7b958ee4fb6222
amck <aaronmckinstry706@gmail.com>
2022-02-12 14:17:49 -0500

    Replaced map with array; massive improvement in runtim (5k seconds to 3k seconds estimated completion time).

--- src/main/java/aaron/wordle/solver/WordConstraints.java
+++ src/main/java/aaron/wordle/solver/WordConstraints.java
@@ -9,7 +9,7 @@ public class WordConstraints {
     private final int wordLength;
     private final Map<Character, Integer> characterToMinCountInWord;
     private final Map<Character, Integer> characterToMaxCountInWord;
-    private final Map<Integer, Set<Character>> positionToAllowedCharacters;
+    private final boolean[][] positionToAllowedCharacters;
 
     private WordConstraints() {
         throw new IllegalStateException();
@@ -19,23 +19,22 @@ public class WordConstraints {
         wordLength = wordConstraints.wordLength;
         characterToMinCountInWord = new HashMap<>(wordConstraints.characterToMinCountInWord);
         characterToMaxCountInWord = new HashMap<>(wordConstraints.characterToMaxCountInWord);
-        positionToAllowedCharacters = new HashMap<>();
-        for (int position : wordConstraints.positionToAllowedCharacters.keySet()) {
-            positionToAllowedCharacters.put(position, new HashSet<>(wordConstraints.positionToAllowedCharacters.get(position)));
+        positionToAllowedCharacters = new boolean[wordConstraints.wordLength][];
+        for (int position = 0; position < positionToAllowedCharacters.length; position++) {
+            positionToAllowedCharacters[position] = Arrays.copyOf(wordConstraints.positionToAllowedCharacters[position], wordConstraints.positionToAllowedCharacters[position].length);
         }
     }
 
     public WordConstraints(int wordLength) {
         this.characterToMinCountInWord = new HashMap<>();
         this.characterToMaxCountInWord = new HashMap<>();
-        this.positionToAllowedCharacters = new HashMap<>();
         this.wordLength = wordLength;
-        for (int i = 0; i < wordLength; ++i) {
-            Set<Character> alphabet = new HashSet<>();
-            for (char c = 'a'; c <= 'z'; c++) {
-                alphabet.add(c);
+        this.positionToAllowedCharacters = new boolean[wordLength][];
+        for (int position = 0; position < positionToAllowedCharacters.length; ++position) {
+            this.positionToAllowedCharacters[position] = new boolean[26];
+            for (int allowedCharacter = 0; allowedCharacter < 26; ++allowedCharacter) {
+                this.positionToAllowedCharacters[position][allowedCharacter] = true;
             }
-            positionToAllowedCharacters.put(i, alphabet);
         }
     }
 
@@ -50,21 +49,22 @@ public class WordConstraints {
         Map<Character, Integer> charToNumResponsesIndicatingCharNotInWord = new HashMap<>();
         for (int guessPosition = 0; guessPosition < constraints.wordLength; ++guessPosition) {
             char guessCharacter = guess.charAt(guessPosition);
+            int guessCharOffset = guessCharacter - 'a';
             PositionResponse responseAtPosition = response.get(guessPosition);
             if (responseAtPosition == PositionResponse.IN_WORD_NOT_POSITION) {
                 constraints.characterToMinCountInWord.put(guessCharacter, Math.max(constraints.characterToMinCountInWord.getOrDefault(guessCharacter, 0), 1));
-                constraints.positionToAllowedCharacters.get(guessPosition).remove(guessCharacter);
+                constraints.positionToAllowedCharacters[guessPosition][guessCharOffset] = false;
                 charToNumResponsesIndicatingCharInWord.put(guessCharacter, charToNumResponsesIndicatingCharInWord.getOrDefault(guessCharacter, 0) + 1);
             } else if (responseAtPosition == PositionResponse.IN_POSITION) {
                 constraints.characterToMinCountInWord.put(guessCharacter, Math.max(constraints.characterToMinCountInWord.getOrDefault(guessCharacter, 0), 1));
                 charToNumResponsesIndicatingCharInWord.put(guessCharacter, charToNumResponsesIndicatingCharInWord.getOrDefault(guessCharacter, 0) + 1);
-                for (char notAllowedChar = 'a'; notAllowedChar <= 'z'; notAllowedChar++) {
-                    if (notAllowedChar == guessCharacter) continue;
-                    constraints.positionToAllowedCharacters.get(guessPosition).remove(notAllowedChar);
+                for (char notAllowedOffset = 0; notAllowedOffset < 26; notAllowedOffset++) {
+                    if (notAllowedOffset == guessCharOffset) continue;
+                    constraints.positionToAllowedCharacters[guessPosition][notAllowedOffset] = false;
                 }
             } else if (responseAtPosition == PositionResponse.NOT_IN_WORD) {
                 charToNumResponsesIndicatingCharNotInWord.put(guessCharacter, charToNumResponsesIndicatingCharNotInWord.getOrDefault(guessCharacter, 0) + 1);
-                constraints.positionToAllowedCharacters.get(guessPosition).remove(guessCharacter);
+                constraints.positionToAllowedCharacters[guessPosition][guessCharOffset] = false;
             } else {
                 throw new IllegalStateException("response.get(" + guessPosition + ") is " + responseAtPosition + ", which is not a recognized response!");
             }
@@ -72,11 +72,12 @@ public class WordConstraints {
 
         for (int guessPosition = 0; guessPosition < constraints.wordLength; ++guessPosition) {
             char guessChar = guess.charAt(guessPosition);
+            int guessCharOffset = guessChar - 'a';
             int inWordCount = charToNumResponsesIndicatingCharInWord.getOrDefault(guessChar, 0);
             int notInWordCount = charToNumResponsesIndicatingCharNotInWord.getOrDefault(guessChar, 0);
             if (inWordCount == 0 && notInWordCount > 0) {
                 for (int answerPosition = 0; answerPosition < constraints.wordLength; answerPosition++) {
-                    constraints.positionToAllowedCharacters.get(answerPosition).remove(guessChar);
+                    constraints.positionToAllowedCharacters[answerPosition][guessCharOffset] = false;
                 }
             } else if (inWordCount > 0 && notInWordCount == 0) {
                 constraints.characterToMinCountInWord.put(guessChar, Math.max(constraints.characterToMinCountInWord.getOrDefault(guessChar, 0), inWordCount));
@@ -97,7 +98,8 @@ public class WordConstraints {
         Map<Character, Integer> characterToCount = new HashMap<>();
         for (int wordIndex = 0; wordIndex < word.length(); wordIndex++) {
             char c = word.charAt(wordIndex);
-            if (!positionToAllowedCharacters.get(wordIndex).contains(c)) {
+            int cOffset = c - 'a';
+            if (!positionToAllowedCharacters[wordIndex][cOffset]) {
                 return false;
             }
             characterToCount.put(c, characterToCount.getOrDefault(c, 0) + 1);
